feab019beca353bcb85fef7469686d4c3e1aed80
ItsCloudxo <sebastian@nnsoft.dk>
2022-03-23 10:59:41 +0100

    Add project files.

---
+++ Number_Swap/Program.cs
@@ -0,0 +1,9 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+Convert.ToDouble(a);
+Convert.ToDouble(b);
+
+(a, b) = (b, a);
+
+Console.WriteLine($"Value a is: {a} but should be {b}");
+Console.WriteLine($"Value b is: {b} but should be {a}");
